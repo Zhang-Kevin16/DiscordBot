1016e75c2b6e23a91bf5b0ecfb77ec50147da750
Zhang-Kevin16 <shunhanzhang@hotmail.com>
2020-01-11 19:07:19 -0800

    print token

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -30,7 +30,9 @@ public class Bot extends ListenerAdapter {
     }
 
     public static  void main(String[] args) throws LoginException, InterruptedException {
-        JDA bot = new JDABuilder("NjY1NDA1MTk0MTMzMDQ1MjU5.XhqJcQ.Gp0-CLGejeQ7_0Jx90RhStamuZA").build();
+        String token = "NjY1NDA1MTk0MTMzMDQ1MjU5.XhqJcQ.Gp0-CLGejeQ7_0Jx90RhStamuZA";
+        System.out.println(token);
+        JDA bot = new JDABuilder(token).build();
         bot.awaitReady();
         bot.addEventListener(new Bot());
     }
